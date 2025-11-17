85ad9a98c65a877ebec22b58f7a85481093f7df4
takahashijake <jtakahashi@ucmerced.edu>
2025-11-17 12:55:15 -0800

    small changes

--- Part1/src/feature.py
+++ Part1/src/feature.py
@@ -4,10 +4,10 @@ import numpy as np
 data = scipy.io.loadmat('../images/MNISTmini.mat')
 
 def printFeaturesShape(data):
-    print(f"{data['train_fea1'].shape}")
-    print(f"{data['train_gnd1'].shape}")
-    print(f"{data['test_fea1'].shape}")
-    print(f"{data['test_gnd1'].shape}")
+    print(f"train_fea1 shape: {data['train_fea1'].shape}")
+    print(f"train_gnd1 shape: {data['train_gnd1'].shape}")
+    print(f"test_fea1 shape: {data['test_fea1'].shape}")
+    print(f"test_gnd1 shape: {data['test_gnd1'].shape}")
 
 
 
