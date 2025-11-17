e3fe9930b5525a8b19f44943142e806e287d8a9b
takahashijake <jtakahashi@ucmerced.edu>
2025-11-17 14:05:10 -0800

    removed unnecessary stashed file

--- Part1/src/test.py
+++
@@ -1,6 +0,0 @@
-testNumber = input("Please enter a test number: ")
-print(f"You entered test number: {testNumber}")
-
-input("Please enter in another number: ")
-
-userSelection = input("Please enter X or O to select your letter: ")
